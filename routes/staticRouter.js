c62c91e15240bb04e382a615c36e79f4ee79a640
Mohim Khan <mohimkhan652@gmail.com>
2023-10-03 14:09:54 +0600

    updated the staticRouter for signup and login

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -2,12 +2,19 @@ const express = require('express');
 const router = express.Router();
 const Url = require('../models/url')
 
-router.get('/', async (req, res)=>{
+router.get('/', async (req, res) => {
     const allUrls = await Url.find({});
     res.render('home', {
         urls: allUrls
     });
 })
 
+router.get('/signup', (req, res) => {
+    res.render('signup');
+})
+
+router.get('/login', (req, res) => {
+    res.render('login');
+})
 
 module.exports = router;
