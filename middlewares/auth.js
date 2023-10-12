67dd28be95e5c8429740c3941f6478fe1adec56e
Mohim Khan <mohimkhan652@gmail.com>
2023-10-12 07:58:15 +0600

    refactor: added role based authentication and check if user is login

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,24 +1,30 @@
 const { getUser } = require('../service/auth');
 
+function checkForAuthentication(req, res, next) {
+    const tokenValue = req.cookies?.token;
+    req.user = null;
 
-async function restrictToLoggedInUserOnly(req, res, next) {
-    const userUid = req.cookies?.uid;
+    if (!tokenValue) return next();
 
-    if (!userUid) return res.redirect('/login');
-    const user = getUser(userUid);
-    if (!user) return res.redirect('/login');
+    const user = getUser(tokenValue);
+
     req.user = user;
-    next();
+
+    return next();
 }
 
-async function checkAuth(req, res, next) {
-    const userUid = req.cookies?.uid;
-    const user = getUser(userUid);
-    req.user = user;
-    next();
+function restrictTo(roles = []) {
+    return function (req, res, next) {
+        if (!req.user) return res.redirect('/login');
+        console.log(req.user)
+        if (!roles.includes(req.user.role)) return res.end("UnAuthorized");
+
+        return next();
+    }
 }
+
 
 module.exports = {
-    restrictToLoggedInUserOnly,
-    checkAuth
+    checkForAuthentication,
+    restrictTo
 }
