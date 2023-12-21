42ea513f20330d3d566eb46ed2bd6cdd4baef27e
Mohim Khan <mohimkhan652@gmail.com>
2023-12-21 22:35:26 +0600

    refactor: increase expiry of token to 1 day

--- service/auth.js
+++ service/auth.js
@@ -6,7 +6,7 @@ function setUser(user) {
         _id: user._id,
         email: user.email,
         role: user.role,
-    }, secret);
+    }, secret, { expiresIn: '1d' });
 }
 
 function getUser(token) {
