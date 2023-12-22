2c0ff0e153efd12e6f00fdd5dcfefe29baf15783
Mohim Khan <mohimkhan652@gmail.com>
2023-12-22 09:56:35 +0600

    remove unnecessary logs

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -16,7 +16,6 @@ function checkForAuthentication(req, res, next) {
 function restrictTo(roles = []) {
     return function (req, res, next) {
         if (!req.user) return res.redirect('/login');
-        console.log(req.user)
         if (!roles.includes(req.user.role)) return res.end("UnAuthorized");
 
         return next();
