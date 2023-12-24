6c3937ec3595457d7790756029e716074e965b9f
Mohim Khan <mohimkhan652@gmail.com>
2023-12-24 09:25:10 +0600

    feat: show all the user details on admin route

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -2,12 +2,24 @@ const express = require('express');
 const router = express.Router();
 const Url = require('../models/url')
 const { restrictTo } = require('../middlewares/auth');
+const User = require('../models/user');
 
 
 router.get('/admin/urls', restrictTo(['ADMIN']), async (req, res) => {
-    const allUrls = await Url.find({});
-    res.render('home', {
-        urls: allUrls
+    const data = [];
+    const user = await User.find({});
+
+    for (let i = 0; i < user.length; i++) {
+        const {_id, name, email, role} = user[i];
+        
+        if (user[i]) {
+            const urls = await Url.find({createdBy: _id});
+            data.push({name, email, role, userDetails: {length: urls.length, links: urls}})
+        }
+    }
+
+    res.render('admin', {
+        data
     });
 })
 
