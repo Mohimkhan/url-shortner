459f0a6275120bcdf62c79ca31d62b3674ddeced
Mohimkhan <mohimkhan652@gmail.com>
2024-09-03 20:56:24 +0600

    chore: remove unwanted imports

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,5 @@
 const User = require('../models/user');
-const { v4: uuidv4 } = require('uuid');
-const { setUser, getUser } = require('../service/auth');
+const { setUser } = require('../service/auth');
 
 async function handleUserSignup(req, res) {
     const { name, email, password } = req.body;
