6634cd395696888036e93951744423fb60809ce8
Mohimkhan <mohimkhan652@gmail.com>
2025-01-03 04:18:25 +0600

    chore: change route name for clarity

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { handleUserSignup, handleUserLogin, handleUserLogout} = require('../controllers/user')
 
-router.post('/', handleUserSignup);
+router.post('/signup', handleUserSignup);
 router.post('/login', handleUserLogin);
 router.get('/logout', handleUserLogout);
 
