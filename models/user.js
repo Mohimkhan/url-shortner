21e18064c06ea0ca88e583a96a441938a21cf9ea
Mohimkhan <mohimkhan652@gmail.com>
2025-01-03 04:14:06 +0600

    feat: hash password for security

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require("bcrypt");
+const { saltOrRounds } = require('../constants');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -22,6 +23,20 @@ const userSchema = new mongoose.Schema({
     }
 }, { timestamps: true });
 
-const User = mongoose.model('user', userSchema);
+userSchema.pre("save", async function (next) {
+    // Check if the password field is modified
+    if (!this.isModified("password")) return next();
+    
+    // Hash the password
+    try {
+        this.password = await bcrypt.hash(this.password, saltOrRounds);
+        next();
+    } catch (err) {
+        next(err); // Pass errors to the next middleware
+    }
+})
+
+const User = mongoose.models.User || mongoose.model('user', userSchema);
+
 
 module.exports = User;
