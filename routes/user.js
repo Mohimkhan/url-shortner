e4e546d184dd55010a0f9d9b43c03549346625fd
Mohimkhan <mohimkhan652@gmail.com>
2025-01-12 06:47:32 +0600

    feat: restrict access to logout route for NORMAL and ADMIN users

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { handleUserSignup, handleUserLogin, handleUserLogout} = require('../controllers/user')
+const { handleUserSignup, handleUserLogin, handleUserLogout} = require('../controllers/user');
+const { restrictTo } = require('../middlewares/auth');
 
 router.post('/signup', handleUserSignup);
 router.post('/login', handleUserLogin);
-router.get('/logout', handleUserLogout);
+router.get('/logout', restrictTo(["NORMAL", "ADMIN"]), handleUserLogout);
 
 module.exports = router;
