7cf9bfd4855b83d21c53e5a138a24cec8f04e5a5
Mohimkhan <mohimkhan652@gmail.com>
2025-01-16 03:16:16 +0600

    feat: add middleware for logout route and simplify access control

--- routes/user.js
+++ routes/user.js
@@ -3,8 +3,14 @@ const router = express.Router();
 const { handleUserSignup, handleUserLogin, handleUserLogout} = require('../controllers/user');
 const { restrictTo } = require('../middlewares/auth');
 
+// middleware
+router.use("/logout", async (req, res, next) => {
+    console.log("yes worked");
+    next();
+});
+
 router.post('/signup', handleUserSignup);
 router.post('/login', handleUserLogin);
-router.get('/logout', restrictTo(["NORMAL", "ADMIN"]), handleUserLogout);
+router.get('/logout', handleUserLogout);
 
 module.exports = router;
