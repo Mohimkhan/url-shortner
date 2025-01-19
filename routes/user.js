7e18c83dbb1d29082708e9630b7a7e312e5ecee9
Mohimkhan <mohimkhan652@gmail.com>
2025-01-19 16:00:25 +0600

    refactor: remove unused middleware for logout route in user routes

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const { handleUserSignup, handleUserLogin, handleUserLogout} = require('../controllers/user');
-const { restrictTo } = require('../middlewares/auth');
-
-// middleware
-router.use("/logout", async (req, res, next) => {
-    console.log("yes worked");
-    next();
-});
 
 router.post('/signup', handleUserSignup);
 router.post('/login', handleUserLogin);
