d283b5571bd79a0a08dabb9d16f720be76e4022a
Mohimkhan <mohimkhan652@gmail.com>
2025-01-26 22:17:27 +0600

    feat: implement copy functionality for short links in home.js

--- public/js/home.js
+++ public/js/home.js
@@ -1,1 +1,17 @@
-console.log("hello from home");
+const shortLinksContainer = document.querySelector(".shortLinks-container");
+const shortId = document.querySelector(".shortId");
+
+shortLinksContainer.addEventListener("click", (event) => {
+  if (event.target.matches(".copyBtn")) {
+    const btn = event.target;
+    const id = btn.parentElement.children[1].textContent.trim();
+    const shortUrl = `${window.location.origin}/url/${id}`;
+    // copy the text to the clipboard
+    navigator.clipboard.writeText(shortUrl);
+    // change the button text
+    btn.textContent = "copied!";
+    setTimeout(() => {
+      btn.textContent = "copy";
+    }, 2000);
+  }
+});
